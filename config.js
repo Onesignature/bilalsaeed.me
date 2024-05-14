4f6f11299d5a5cbf652c0a1ff84a2ead70fe1a70
Bilal Saeed <bilalsaeedahmed1@gmail.com>
2024-05-14 16:28:02 +0400

    Updated with new sections, just need to add IMages now

--- config.js
+++ config.js
@@ -16,6 +16,125 @@ window.PorfolioConfig = {
       description: 'With fluency in C and C++, Ive ventured into learning Solidity, enabling me to deploy my cryptocurrencies, experiment with smart contracts, and identify vulnerabilities for reporting. This journey encompasses a deep dive into blockchain technology, allowing me to innovate and contribute to the security and functionality of decentralized systems.'
     }
   ],
+  achievements: [
+    {
+      name: 'Botim Hackathon winners',
+      description: 'Attended Botim Hackathon on their new rebrand event and managed to propose a blockchain implementation for scaling Botims remittance solutions.'
+    },
+    {
+      name: '42 0X Hackathon Winners',
+      description: 'Winning the intensive 42 Hackathon which ran for 3 days; Me and my team developed an application for streamlining university operations such as checking in, registering for food and events.'
+    }
+  ],
+  challenges: [
+    {
+      name: 'Complex Debugging in Blockchain Applications',
+      description: 'Faced with intricate debugging challenges in decentralized applications, I developed a systematic approach to trace and resolve issues in smart contract code, enhancing system reliability and security.'
+    },
+    {
+      name: 'Performance Optimization in Web Development',
+      description: 'Tackled the challenge of optimizing web application performance, reducing load times by implementing efficient code practices and leveraging advanced web technologies.'
+    }
+  ],
+  goals: [
+    {
+      name: 'Short Term',
+      description: 'To obtain a certification in Advanced Solidity Development by the end of the year, enhancing my capabilities in smart contract development. Expand my portfolio with at least three more innovative web development projects focusing on user experience and blockchain integration.'
+    },
+    {
+      name: 'Performance Optimization in Web Development',
+      description: 'Tackled the challenge of optimizing web application performance, reducing load times by implementing efficient code practices and leveraging advanced web technologies.'
+    },
+    {
+      name: '42 Education',
+      description: 'To complete my final 42 Project Ft_transcendence and continue my education with 42 during the Mastery stage'
+    },
+    {
+      name: 'Long Term',
+      description: 'Aim to lead a development team on a major blockchain project, focusing on creating scalable and secure decentralized applications. Aspire to contribute to major open-source blockchain platforms, improving their architecture and community engagement.'
+    }
+  ],
+  reflections: [
+    {
+      name: 'On Projects',
+      description: 'Reflecting on my recent projects, including those developed during hackathons and as part of my portfolio at 42 school, Ive realized the importance of adaptability and user-centered design. Iterative testing and feedback have been crucial in refining the functionalities to meet user expectations efficiently.'
+    },
+    {
+      name: 'On Education at 42 School',
+      description: 'The unique educational approach at 42 school, focusing on peer-to-peer learning and project-based tasks, has significantly shaped my problem-solving skills. This environment fostered a profound understanding of practical application in software development and the ability to learn independently and collaboratively.'
+    },
+    {
+      name: 'On Peer-to-Peer Learning',
+      description: 'Peer-to-peer learning has not only accelerated my learning curve but also enhanced my communication skills. Working closely with peers has allowed for a deeper exchange of ideas and has instilled a strong sense of community and teamwork, essential for any successful tech professional.'
+    }
+  ],
+  projects: [
+    {
+      name: 'Libft',
+      description: 'My first project at 42, where I built a C library by re-coding several standard C library functions along with some additional ones. This project helped me understand the intricacies of C programming, memory management, and the importance of well-documented code.'
+    },
+    {
+      name: 'NetPractice',
+      description: 'A project that involved creating network applications. This taught me about socket programming and network protocols, providing a practical foundation in building and securing networked applications.'
+    },
+    {
+      name: 'Piscine C',
+      description: 'An intensive coding bootcamp that tested my resilience and ability to learn quickly. The fast-paced environment of this project honed my skills in C programming and algorithmic thinking under tight deadlines.'
+    },
+    {
+      name: 'Ft_printf',
+      description: 'A challenging project where I recreated the printf function. This project deepened my understanding of variadic functions in C and improved my debugging skills, as well as precision in implementing complex software solutions.'
+    },
+    {
+      name: 'Philosophers',
+      description: 'In this project, I tackled the problem of dining philosophers with a focus on processes and threads. It was instrumental in teaching me about concurrency, thread management, and synchronization techniques to avoid deadlocks.'
+    },
+    {
+      name: 'Cub3D',
+      description: 'This project involved creating a 3D graphical environment similar to the early 90s game Wolfenstein 3D using raycasting technology. It not only enhanced my graphics programming skills but also taught me about spatial thinking and optimization strategies.'
+    }
+  ],
+  softSkills: [
+    {
+      name: 'Problem Solving',
+      description: 'Advanced problem-solving skills, capable of breaking down complex issues into manageable parts and crafting effective solutions under pressure. Regularly demonstrated during intense hackathons and collaborative projects at 42 school.'
+    },
+    {
+      name: 'Team Collaboration',
+      description: 'Highly skilled in teamwork and communication, having worked on numerous group projects at 42 school and in professional settings. I thrive in environments that require coordination and mutual support among team members.'
+    },
+    {
+      name: 'Adaptability',
+      description: 'Quick to adapt to new challenges and changes in technology, which is essential in the fast-paced field of blockchain and web development. My ability to pivot and learn new tools swiftly has been key to maintaining project timelines and quality.'
+    },
+    {
+      name: 'Time Management',
+      description: 'Efficient in managing time and resources to meet deadlines without compromising quality. My training at 42 school, where projects often had tight deadlines, has honed this skill extensively.'
+    },
+    {
+      name: 'Communication',
+      description: 'Effective communicator, able to clearly convey complex technical concepts to non-technical stakeholders. This skill has been invaluable in collaborative projects and in presenting at hackathons and conferences.'
+    },
+    {
+      name: 'Leadership',
+      description: 'Proven leadership capabilities, often taking the lead in group projects and guiding teams towards common goals. My leadership skills were particularly evident during collaborative projects and hackathons, where I was responsible for strategy and execution.'
+    }
+  ],
+  qualifications: [
+    {
+      name: 'Microsoft 365 Certification',
+      description: 'Certified in Microsoft 365, demonstrating advanced skills in utilizing Microsofts suite of productivity applications to enhance workplace efficiency and collaboration.'
+    },
+    {
+      name: 'A Levels and IGCSE',
+      description: 'Achieved A grades in both A Levels and IGCSE examinations, with a focus on Business and Science subjects. This strong academic foundation has contributed to a robust understanding of both scientific principles and business operations.'
+    },
+    {
+      name: 'Microsoft Teams Certification',
+      description: 'Holds a certification in Microsoft Teams, specializing in leveraging this platform for effective team collaboration, communication, and project management in professional environments.'
+    }
+  ],
+      
   works: [
     {
       name: 'Vue Stripe Checkout',
@@ -57,7 +176,7 @@ window.PorfolioConfig = {
       },
       {
         name: 'Linkedin',
-        link: 'https://facebook.com/Onesignature'
+        link: 'https://www.linkedin.com/in/onesignature/'
       },
       {
         name: 'Twitter/X',
